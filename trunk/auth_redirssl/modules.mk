896a70d22a680509cb32e9483da627346925612e
nneul <nneul@31113ff5-ec01-0410-aacd-f3243e021b54>
2010-06-02 01:33:36 +0000

    remove auth_redirssl - it never worked right, and isn't actually being used anywhere
    git-svn-id: https://svn.mst.edu/project/apache@126 31113ff5-ec01-0410-aacd-f3243e021b54

--- trunk/auth_redirssl/modules.mk
+++
@@ -1,4 +0,0 @@
-mod_auth_redirssl.la: mod_auth_redirssl.slo
-	$(SH_LINK) -rpath $(libexecdir) -module -avoid-version  mod_auth_redirssl.lo
-DISTCLEAN_TARGETS = modules.mk
-shared =  mod_auth_redirssl.la
